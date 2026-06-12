28cf32a672941bdd828ea4b93896d0ed77d7e7b9
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2026-06-12 20:38:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "darkacademiawizard-v2";
+const CACHE = "darkacademiawizard-v3";
 
 const FILES = [
   "./",
@@ -10,15 +10,29 @@ const FILES = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
     })
   );
 });
